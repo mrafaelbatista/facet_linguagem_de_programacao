f1496750ac8401053782e84dc5d3cc8c867d1086
Messias Batista <mrafaelbatista@gmail.com>
2019-09-11 20:20:56 -0300

    Aula 06

--- Aula_06/heranca/sample_eletric_car.py
+++ Aula_06/heranca/sample_eletric_car.py
@@ -1,10 +1,10 @@
 class Car():
 
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, year, odometer_reading=0):
         self.make = make
         self.model = model
         self.year = year
-        self.odometer_reading = 0
+        self.odometer_reading = odometer_reading
 
     def get_descriptive_name(self):
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
@@ -22,11 +22,24 @@ class Car():
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
+    def fill_gas_tank(self):
+        print("Tanque do Carro Normal")
+
 
 class ElectricCar(Car):
+
     def __init__(self, make, model, year):
-        super().__init__(make, model, year)
+        super().__init__(make, model, year, odometer_reading=0)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def fill_gas_tank(self):
+        print("Carros elétricos não têm tanque de combustível")
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
---
+++ Aula_06/importing_class/car.py
@@ -0,0 +1,56 @@
+# Esse módulo tenta representar um caror
+
+class Car():
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, milage):
+        if milage >= self.odometer_reading:
+            self.odometer_reading = milage
+        else:
+            print("You can't roll back an odometer")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+    def fill_gas_tank(self):
+        print("Tanque do Carro Normal")
+
+class Battery():
+
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        # aqui estamos criando uma instância de bateria
+        # dentro da classe ElectricCar
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        print("Carros elétricos não têm tanque de combustível")
